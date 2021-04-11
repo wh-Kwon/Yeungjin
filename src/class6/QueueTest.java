333fc6151ca47068303af247ed7a64879e0cce21
owner <owner@192.168.11.2>
2021-04-11 18:45:49 +0900

    Queue and Map practice

---
+++ src/class6/QueueTest.java
@@ -0,0 +1,19 @@
+package class6;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueTest {
+
+	public static void main(String[] args) throws InterruptedException {
+		int time = 10;
+		Queue<Integer> queue = new LinkedList<Integer>();
+		for(int i = time; i >= 0; i--)
+			queue.add(i);
+		while(!queue.isEmpty()) {
+			System.out.print(queue.remove() + " ");
+			Thread.sleep(1000);
+		}
+	}
+
+}
